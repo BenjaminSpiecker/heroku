a562442903f04622369d3559020c4ad0fa7b3c19
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-30 04:59:12 -0300

    'commit'

--- index.ts
+++ index.ts
@@ -2,15 +2,16 @@ import app from './src/app'
 import {sequelize} from './src/db'
 import bootstrap from './src/bootstrap'
 import User from './src/models/Usuario'
+import config from './src/lib/config'
 sequelize
 .sync({force:true, logging:false})
 .then(() => {
 
     bootstrap()
     console.log('db connected')
-    app.listen(3001, () => {
+    app.listen(config.port, () => {
         
-        console.log('app listening on port 3001')
+        console.log(`app listening on port ${config.port}`);
         
     })
 
